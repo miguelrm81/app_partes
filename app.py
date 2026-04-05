2bfa1a26675c1e0505101225d11de317a3d3a726
miguelrm81 <miguelrm@live.com>
2026-04-05 11:23:16 +0200

    Commit inicial

---
+++ app.py
@@ -0,0 +1,15 @@
+from routes.parte_obra_routes import parte_bp
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__, template_folder="templates", static_folder="static")
+app.secret_key = os.getenv("SECRET_KEY")
+
+app.register_blueprint(parte_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
